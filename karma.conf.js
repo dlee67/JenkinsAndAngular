e65f57439f033979e4c9ba2ac0d01910399a892f
Su-ryun <dlee67@msudenver.edu>
2019-12-27 20:34:26 -0700

    Added bunch of node modules.:

--- karma.conf.js
+++ karma.conf.js
@@ -17,13 +17,17 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['dots', 'junit'],
+    junitReporter = {
+      outputDir: 'karma-results',
+      outputFile: 'karma-results.xml'
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: false,
+    browsers: ['PhantomJS'],
+    singleRun: true,
     restartOnFileChange: true
   });
 };
