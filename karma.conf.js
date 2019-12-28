cb43299dade46956bf82b3940a409ffa8b2b3859
Su-ryun <dlee67@msudenver.edu>
2019-12-27 20:49:12 -0700

    Added karma-phantomjs-launcher in plugins.

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-junit-reporter'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
