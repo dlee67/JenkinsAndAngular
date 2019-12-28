343d58dab0702a15053900ae05c217f753ecb8c9
Su-ryun <dlee67@msudenver.edu>
2019-12-27 19:52:44 -0700

    Reverted everything back to the intial karma conf file you get when ng init.

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,9 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-junit-reporter'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
@@ -17,22 +13,16 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/my-app'),
+      dir: require('path').join(__dirname, './coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml', 'junit'],
-    junitReporter: { 
-      outputDir: 'karma-results',
-      outputFile: 'karma-results.xml'
-    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    // browsers: ['ChromeHeadless'],
-    autoWatch: false,
     singleRun: false,
     restartOnFileChange: true
   });
